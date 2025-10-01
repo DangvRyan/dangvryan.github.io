0b47fe0772af5092428c5937637ace818ffc3572
DangvRyan <dangvryan@gmail.com>
2025-09-30 21:59:12 -0400

    2025 WIP

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 document.querySelector('.logo').addEventListener('mouseover', changeimage);
 document.querySelector('.logo').addEventListener('mouseout', changeimage2);
 
+/*
 function changeimage() {
    let image= document.querySelector('.logo').src = 'assets/logo2.png';
 }
@@ -8,6 +9,7 @@ function changeimage() {
 function changeimage2() {
     let image= document.querySelector('.logo').src = 'assets/logo1.png';
 }
+*/
 
 function setupRollover() {
     var images = document.querySelectorAll('.roll');
